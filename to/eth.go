d1687cd6dc6ebd0c72c0d9df478abeb6f2233b5a
Jesse Kuang <jkuang@21cn.com>
2023-11-07 19:12:56 +0800

    ether may exceed uint64 limits

--- to/eth.go
+++ to/eth.go
@@ -1,5 +1,7 @@
 package to
 
+import "math/big"
+
 const (
 	gwei  = 1e9
 	ether = 1e18
@@ -9,14 +11,17 @@ func ToGWei(v uint64) float64 {
 	return float64(v) / gwei
 }
 
-func ToEther(v uint64) float64 {
-	return float64(v) / ether
+func ToEther(v *big.Int) float64 {
+	dd, _ := v.Float64()
+	return dd / ether
 }
 
 func FromGWei(v float64) uint64 {
 	return uint64(v * gwei)
 }
 
-func FromEther(v float64) uint64 {
-	return uint64(v * ether)
+func FromEther(v float64) *big.Int {
+	ret := new(big.Int)
+	ret.SetUint64(uint64(v * gwei))
+	return ret.Mul(ret, big.NewInt(gwei))
 }
